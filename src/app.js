fe7ecaa3d59b6cc8967884b8377aa682edcafad9
faithorozco <faith_orozco@yahoo.com>
2021-10-07 11:48:50 -0700

    following frontend masters tutorial videos, added prettier to project

--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-
-    ])
-}
-
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './pet';
 
 const App = () => {
-    return React.createElement(
-        "div", 
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {name: 'Frito', animal: 'dog', breed: 'chiweenie'}),
-            React.createElement(Pet, {name: 'speck', animal: 'dog', breed: 'australian sheperd'}),
-            React.createElement(Pet, {name: 'dorian', animal: 'cat', breed: 'orange'}),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Frito",
+      animal: "dog",
+      breed: "chiweenie",
+    }),
+    React.createElement(Pet, {
+      name: "speck",
+      animal: "dog",
+      breed: "australian sheperd",
+    }),
+    React.createElement(Pet, {
+      name: "dorian",
+      animal: "cat",
+      breed: "orange",
+    }),
+  ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
